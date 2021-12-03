1d7d982d669c52a603c2e4e59d0b5cf3c373314f
Betcy Babu <aleenababu93@gmail.com>
2021-12-03 22:50:38 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,90 @@
+variable "project" {
+
+    default = "Enter Project Name"
+}
+
+variable "region" {
+
+    default = "ap-south-1"
+
+}
+
+variable "access_key" {
+
+    default = "Enter Access Key"
+
+}
+
+variable "secret_key" {
+
+    default = "Enter Secret Key"
+
+}
+
+variable "cidr_block" {
+
+    default = "172.16.0.0/16"
+}
+
+variable "cidr_block_pub01" {
+
+    default = "172.16.0.0/19"
+}
+
+variable "AZ_pub01" {
+
+    default = "ap-south-1a"
+}
+
+variable "cidr_block_pub02" {
+
+    default = "172.16.32.0/19"
+}
+
+variable "AZ_pub02" {
+
+    default = "ap-south-1b"
+}
+
+variable "cidr_block_pub03" {
+
+    default = "172.16.64.0/19"
+}
+
+variable "AZ_pub03" {
+
+    default = "ap-south-1c"
+}
+
+variable "cidr_block_pri01" {
+
+    default = "172.16.96.0/19"
+}
+
+variable "AZ_pri01" {
+
+    default = "ap-south-1a"
+}
+
+variable "cidr_block_pri02" {
+
+    default = "172.16.128.0/19"
+}
+
+variable "AZ_pri02" {
+
+    default = "ap-south-1b"
+}
+
+variable "cidr_block_pri03" {
+
+    default = "172.16.160.0/19"
+}
+
+variable "AZ_pri03" {
+
+    default = "ap-south-1c"
+}
+
+
+
